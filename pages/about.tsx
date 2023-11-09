949bb275f65fe3be1163100927a755001938171d
Md Shamsuzzama Siddique <75604504+mdshamsh20@users.noreply.github.com>
2023-11-09 11:11:08 +0530

    about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -75,7 +75,7 @@ export default function About() {
     <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
       <article className="flex max-w-xl flex-col items-start justify-between">
         <div className="flex items-center gap-x-4 text-xs">
-          <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
+          <time  className="text-gray-500" >Mar 16, 2020</time>
           <a href="#" className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
         </div>
         <div className="group relative">

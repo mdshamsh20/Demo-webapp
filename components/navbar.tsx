13f9d5aa83848feae3bbd97f0fc83649f1cafac0
Md Shamsuzzama Siddique <75604504+mdshamsh20@users.noreply.github.com>
2023-11-09 11:07:11 +0530

    navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -43,10 +43,10 @@ export default function Navbar() {
           {
               toggle &&
 
-          <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+          <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" >
            
-            <Link href="/profile" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</Link>
-            <Link href="/signup" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</Link>
+            <Link href="/profile" className="block px-4 py-2 text-sm text-gray-700" role="menuitem"  id="user-menu-item-0">Your Profile</Link>
+            <Link href="/signup" className="block px-4 py-2 text-sm text-gray-700" role="menuitem"  id="user-menu-item-2">Sign out</Link>
           </div>
           }
     
@@ -64,11 +64,11 @@ export default function Navbar() {
             
               toggle1 &&
 
-          <div className="absolute right-0 z-10  mr-6 mt-48 w-24 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+          <div className="absolute right-0 z-10  mr-6 mt-48 w-24 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" >
            
-            <Link href="/" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Home</Link>
-            <Link href="/about" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">About</Link>
-            <Link href="/nft" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">NFT</Link>
+            <Link href="/" className="block px-4 py-2 text-sm text-gray-700" role="menuitem"  id="user-menu-item-0">Home</Link>
+            <Link href="/about" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-1">About</Link>
+            <Link href="/nft" className="block px-4 py-2 text-sm text-gray-700" role="menuitem"  id="user-menu-item-2">NFT</Link>
           </div>
           }
            
